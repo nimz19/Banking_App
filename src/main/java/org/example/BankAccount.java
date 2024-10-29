adc82df01dbb1fdbb0d317661d5568a86eb1ac49
nimz19 <izzahzafer@gmail.com>
2024-10-29 15:31:17 +0000

    Created BankAccount class with balance, min balance, accountholdername properties & have deposit and withdraw behaviours

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -2,4 +2,45 @@ package org.example;
 
 public class BankAccount {
     private String AccountHolderName;
+    private double Balance;
+    private double MinimumBalance;
+
+    public BankAccount(String accountHolderName, double balance, double minimumBalance) {
+        AccountHolderName = accountHolderName;
+        Balance = balance;
+        MinimumBalance = minimumBalance;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+
+    public String getAccountHolderName() {
+        return AccountHolderName;
+    }
+
+    //method to deposit and update amount
+    public void deposit(double amount) {
+        if (amount > 0) {
+            Balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (Balance - amount >= MinimumBalance) {
+            Balance -= amount;
+        } else {
+            throw new IllegalArgumentException("Cannot withdraw below minimum balance.");
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "AccountHolderName='" + AccountHolderName + '\'' +
+                ", Balance=" + Balance +
+                ", MinimumBalance=" + MinimumBalance +
+                '}';
+    }
 }
